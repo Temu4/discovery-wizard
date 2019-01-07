375b31f55c32c6fcbfcdb5f3df09983bc6178ae5
Temu4 <artem.vorobjov@gmail.com>
2019-01-07 16:58:34 +0200

    Recoding with Steps UI

--- src/script.js
+++ src/script.js
@@ -1,11 +1,54 @@
+// Accordion
 $(".ui.accordion").accordion();
 
+// Dropdown
 $(".dropdown").dropdown();
 
-$("#target").select(function() {
-  $(".button").toggle();
+// Discovery Source changing content
+$("#ip").click(function() {
+  $("#ip-content").show();
 });
 
-$("#target").select(function() {
-  alert("Handler for .select() called.");
+$("#ip-range").click(function() {
+  $("#ip-range-content").show();
+});
+
+$("#cvs").click(function() {
+  $("#cvs-content").show();
+});
+
+$("#ip").click(function() {
+  if ($("#ip-range-content").show()) {
+    $("#ip-range-content").hide();
+  }
+});
+
+$("#ip").click(function() {
+  if ($("#cvs-content").show()) {
+    $("#cvs-content").hide();
+  }
+});
+
+$("#ip-range").click(function() {
+  if ($("#ip-content").show()) {
+    $("#ip-content").hide();
+  }
+});
+
+$("#ip-range").click(function() {
+  if ($("#cvs-content").show()) {
+    $("#cvs-content").hide();
+  }
+});
+
+$("#cvs").click(function() {
+  if ($("#ip-content").show()) {
+    $("#ip-content").hide();
+  }
+});
+
+$("#cvs").click(function() {
+  if ($("#ip-range-content").show()) {
+    $("#ip-range-content").hide();
+  }
 });
